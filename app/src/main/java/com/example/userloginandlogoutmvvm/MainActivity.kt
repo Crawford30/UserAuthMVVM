80e334f36b0ef2efd94773d7f0bdab71d4d860aa
Joel Crawford <macbook@Crawford.local>
2023-03-23 16:04:54 +0300

    User login design

--- app/src/main/java/com/example/userloginandlogoutmvvm/MainActivity.kt
+++ app/src/main/java/com/example/userloginandlogoutmvvm/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.userloginandlogoutmvvm
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.userloginandlogoutmvvm.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+
+
+       val intent = Intent(this, AuthActivity::class.java)
+
+        //val intent = Intent(AuthActivity::class.java)
+
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/userloginandlogoutmvvm/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.userloginandlogoutmvvm.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.userloginandlogoutmvvm.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}

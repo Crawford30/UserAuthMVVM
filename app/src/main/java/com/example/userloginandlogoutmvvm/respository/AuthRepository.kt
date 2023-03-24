246698d44f78109a83acc59852b8de905a212d6a
Joel Crawford <macbook@Crawford.local>
2023-03-24 08:48:54 +0300

    User Respository

---
+++ app/src/main/java/com/example/userloginandlogoutmvvm/respository/AuthRepository.kt
@@ -0,0 +1,13 @@
+package com.example.userloginandlogoutmvvm.respository
+
+import com.example.userloginandlogoutmvvm.network.AuthApi
+
+class AuthRepository(private val api: AuthApi) : BaseRepository() {
+
+    //login function that will call the safe api call
+   suspend fun login(email:String, password:String) =  safeApiCall {
+        api.login(email, password)
+    }
+
+
+}

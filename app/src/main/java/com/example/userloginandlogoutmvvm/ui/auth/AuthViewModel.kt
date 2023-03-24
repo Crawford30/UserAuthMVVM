026ea024873c7d8e9876d7c50ec92bd88e328d07
Joel Crawford <macbook@Crawford.local>
2023-03-24 11:19:31 +0300

    View Model factory

---
+++ app/src/main/java/com/example/userloginandlogoutmvvm/ui/auth/AuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.userloginandlogoutmvvm.ui.auth
+
+import androidx.lifecycle.ViewModel
+import com.example.userloginandlogoutmvvm.respository.AuthRepository
+
+class AuthViewModel(private  val repository: AuthRepository): ViewModel() {
+    //the view model communicates with the repository
+    //By the help of repository we call the function
+
+
+}
--- app/src/main/java/com/example/userloginandlogoutmvvm/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/userloginandlogoutmvvm/ui/base/BaseFragment.kt
@@ -6,11 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import com.example.userloginandlogoutmvvm.network.RemoteDataSource
 import com.example.userloginandlogoutmvvm.respository.BaseRepository
 
 abstract class BaseFragment<VM:ViewModel,B:ViewBinding, R: BaseRepository>: Fragment() {
     protected lateinit var binding: B
+    protected lateinit var viewModel: VM
+
+    //instance of remote data source, to create api instance
+    protected  val remoteDataSource = RemoteDataSource()
+
 
 
     override fun onCreateView(
@@ -19,6 +26,16 @@ abstract class BaseFragment<VM:ViewModel,B:ViewBinding, R: BaseRepository>: Frag
         savedInstanceState: Bundle?
     ): View? {
         binding = getFragmentBinding(inflater, container)
+
+        //view model instance
+
+
+        //we need a factory instance
+        val factory = ViewModelFactory(getFragmentRepository())
+
+        //we can create view model instance
+        viewModel = ViewModelProvider(this,factory).get(getViewModel())
+
 
         return binding.root
         //return super.onCreateView(inflater, container, savedInstanceState)
